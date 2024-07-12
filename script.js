db99736147a889ba9360f63e9f78b67035f2373e
sher1w <114788994+sher1w@users.noreply.github.com>
2024-07-12 12:13:40 +0530

    new
    game

---
+++ script.js
@@ -0,0 +1,214 @@
+/* let js = "a";
+if (js === "a") {
+  alert("Das is gud");
+}
+console.log(4 + 4 + 5 + 1);
+console.log("jonas");
+console.log(23);
+
+let firstname = "jonas";
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+let javad = true;
+console.log(typeof true)
+console.log(typeof javad);
+console.log(typeof 23);
+console.log( typeof "name")
+console.log(typeof null);
+const job ; 
+let age = 90;
+age = 99;
+
+const birthyear = 1991 ;
+//birthyear =11112;
+const age = 2037  -1991 ;
+const ageS= 2037 - 2018 ; 
+console.log(age , ageS);
+console.log(age * 3, ageS /  2 ** 3 ) // 2**3 means 2 ^ 3 = 8 
+const  firstName = "sherwinn ";
+const secondName=" mazarello ";
+console.log(firstName + secondName);
+console.log( firstName + ' ' + secondName);
+console.log( typeof firstName);
+let x = 10 + 5 ;
+x += 10 ;
+x *= 4 ;
+x--;
+x++;
+console.log(x);
+
+console.log(age > ageS);
+console.log(10 -10 );
+age=111;
+ageS=23;
+const averAge = (age + ageS) / 2 ;
+console.log(  age , averAge , ageS);
+let x , y ;
+x = y = 25 - 10 -5 ;
+console.log(x , y);
+
+//challenge 1 
+const markMass1 =  78 ;
+const markMass2 = 95  ;
+const  markHeigth= 1.69 ;
+const markHeigth2 = 1.88;
+const johnMass1 =  92 ;
+const johnMass2 =  85 ;
+const  johnHeigth= 1.95 ;
+const johnHeigth2 = 1.76;
+function BMI( mass , height)
+{
+
+  return mass/ height  ** 2 
+
+}
+let J1 = BMI(markMass1, markHeigth );
+let J2 = BMI(markMass2, markHeigth2 );
+let M1 = BMI(johnMass1, johnHeigth );
+let M2 = BMI(johnMass2, johnHeigth2 );
+let boolm = J2>M2;
+console.log(boolm);
+console.log( J1,   J2 ,M1 ,M2 );
+const firstName = 'Sherwin';
+const secondName = 'Mazarello';
+const birthYear = 1991;
+
+const sherwin = "I am Sherwin Mazarello. I am " + (new Date().getFullYear() - birthYear) + " years old.";
+console.log(sherwin);
+
+const jason = 'iam  ${firstName} , a  ${birthyear} year old ' ; 
+console.log(jason);
+console.log('just regular sting..');
+console.log('string with \n\ hello \n\ byye \n\ going \n\  ');
+console.log(' rting  satisfies ');
+console.log(` rting  satisfies 
+horse
+cat
+king kong 
+godzilla `);
+
+const age = 10;
+const isold =age >= 18
+if(age>= 18)//true or false
+{ console.log('😎😉🤷‍♂️🤷‍♀️')}
+else
+{const yearleft=18 - age;
+  console.log(`bad time to start 👀✔✔🐱‍🚀🐱‍👓 ${yearleft}`);
+
+}
+if(isold)
+{
+  console.log('sherwin can start 🤞🤞🤞✌');
+}
+
+const birthyear =1991 ;
+if(birthyear <= 2000)
+{let century  =20 ;
+  console.log(`suprise mf  ${century}  `);}
+else{
+  let century = 21;
+  console.log(`suprise mf  ${century}  `);
+}
+let m =  "2345"
+console.log(Number (m) ,  m );
+console.log( Number(m)+1)
+console.log(Number('sherwin'));
+console.log(typeof NaN);
+//console.log(string(23) , 23);
+
+//type cohesion
+console.log('IM'+ 23 + 'YEARS OLD')
+
+console.log('23'+'34'+'44');
+console.log('23'-'10'-3);
+console.log('23' * '2');
+console.log('23'/5);
+
+
+let n ='1'+ 1 +1 +1 ;
+console.log(n -1 );
+// 5 falsy values : 0 ,'' , undefined , null ,NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('jesus'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+
+const money =  12;
+if(money )
+{
+  console.log("rUN")
+}else
+{
+console.log(" INTO ")
+
+}
+
+
+let height = 929
+if(height)
+{
+  console.log(" nigga")
+}
+else
+{
+  console.log("whttttttttt");
+}
+const age = ' 18 ';
+if(age === 18)
+console.log( " y are a dog 🐱‍👤🤳🤳🤳🎂");
+
+
+if(age == 18)
+console.log( " y are a fffdog 🐱‍👤🤳🤳🤳🎂");
+
+const fav = prompt("ur fav number ");
+console.log(fav);
+console.log(typeof fav);
+if(fav == 13)
+{
+  console.log("cooooololo")
+}
+else
+{
+  console.log("the dued");
+}
+const licence = true ;
+const gg = true ;
+console.log(licence && gg)
+console.log(licence ||  gg)
+console.log(!licence)
+console.log(!gg)
+if(licence && gg)
+{
+console.log("get out ")
+}
+else
+{
+console.log("get down ")
+const day = prompt("entwe day reeee");
+switch(day){
+case 'monday' :
+  console.log("nigga whattt");
+  break;
+  case 'tuesday' :
+  console.log("nidaasasasas");
+  breeak ; 
+  default :
+  console.log("bugger off ");}
+}
+const age = 23;
+age  >= 18 ? console.log(" why 🐱‍🏍🎉🌹🎉🐱‍🏍🎉🐱‍🏍🐱‍🏍🐱‍🏍🐱‍💻🐱‍💻 whty") : console.log("niegro ");
+*/
+
+
+
+
+
+
+
